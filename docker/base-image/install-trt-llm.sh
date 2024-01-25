fb48a35fe69812fef45ebb04c9a2be1296bbb142
Jakub Piotr Cłapa <jpc@collabora.com>
2024-01-25 09:40:47 +0100

    Added support for the 3090

--- docker/base-image/install-trt-llm.sh
+++ docker/base-image/install-trt-llm.sh
@@ -4,7 +4,7 @@ export ENV=${ENV:-/etc/shinit_v2}
 source $ENV
 
 cd /root/TensorRT-LLM
-python3 scripts/build_wheel.py --clean --cuda_architectures "89-real;90-real" --trt_root /usr/local/tensorrt
+python3 scripts/build_wheel.py --clean --cuda_architectures "86-real;89-real;90-real" --trt_root /usr/local/tensorrt
 pip install build/tensorrt_llm-0.7.1-cp310-cp310-linux_x86_64.whl
 mv examples ../TensorRT-LLM-examples
 cd ..
